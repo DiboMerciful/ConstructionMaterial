1a476aa7c0bd434b5ce145b53d76ad2852bca022
Dibo-Merciful <dibomerciful4@gmail.com>
2025-02-14 19:27:38 -0800

    User

---
+++ src/main/java/com/mycompany/constructionmaterialstockmanagementapplication/ba2a/ConstructionMaterialStockManagementApplicationBA2A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.constructionmaterialstockmanagementapplication.ba2a;
+
+/**
+ *
+ * @author DIBO MERCIFUL
+ */
+import UI.User;
+public class ConstructionMaterialStockManagementApplicationBA2A {
+
+    public static void main(String[] args) {
+        User log = new User();
+        log.setVisible(true);
+    }
+}
